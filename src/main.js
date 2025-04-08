4ca54d4e7a4e154f282620bb48bc17d4b212e1c5
kravetsiv <brightsky@yandex.ru>
2025-04-08 21:35:45 +0500

    Первичный шаблон с Quasar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/style.css'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+import { usePluginQuasar } from '@/plugins/quasar'
+import { usePluginRouter } from '@/plugins/router'
+
+const app = createApp(App)
+
+usePluginRouter(app)
+usePluginQuasar(app)
+
+app.mount('#app')
