5114420ee20c5d280fb162e43bace1c1ccdfd6b4
Kravets.IV <brightsky@yandex.ru>
2025-05-08 15:11:29 +0500

    Добавлена иконка пользователя и меню выхода

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,26 @@ const core = useCoreStore()
     <q-toolbar>
       <q-btn flat @click="core.drawer = !core.drawer" round dense icon="menu" />
       <q-toolbar-title>ИНМ: База знаний</q-toolbar-title>
+      <q-btn icon="account_circle" rounded dense flat>
+        <q-menu>
+          <q-list>
+            <q-item>
+              <q-item-section class="text-body2 text-blue-10">
+                Иванов Пётр Сидорович
+              </q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item to="/login">
+              <q-item-section avatar>
+                <q-icon name="logout" />
+              </q-item-section>
+              <q-item-section>
+                Завершить сеанс
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
     </q-toolbar>
   </q-header>
 </template>
