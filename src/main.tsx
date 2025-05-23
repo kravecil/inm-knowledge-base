c34da357acf96c714329c765b63a86613f433090
kravetsiv <brightsky@yandex.ru>
2025-05-23 14:52:26 +0500

    CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 import './fonts.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 )
