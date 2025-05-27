c25416b0f1bef20393da124448a3e4fbf8cffb0c
kravetsiv <brightsky@yandex.ru>
2025-05-27 19:47:43 +0500

    Добавлен валидатор для parentId

--- backend/api/schemas/divisions.py
+++ backend/api/schemas/divisions.py
@@ -1,11 +1,26 @@
+from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import AfterValidator, BaseModel, Field
+
+PARENT_UUID = UUID("00000000-0000-0000-0000-000000000000")
+
+
+def replace_whitespaced(value: UUID) -> UUID | None:
+    if value != PARENT_UUID:
+        return value
+
+    return None
 
 
 class DivisionSchema(BaseModel):
     GUID: UUID = Field(validation_alias="GUID", serialization_alias="id")
     name: str = Field(validation_alias="Name", serialization_alias="name")
-    parent_guid: UUID = Field(
-        validation_alias="ParentGuid", serialization_alias="parentId"
-    )
+    parent_guid: Annotated[
+        UUID | None,
+        Field(
+            validation_alias="ParentGuid",
+            serialization_alias="parentId",
+        ),
+        AfterValidator(replace_whitespaced),
+    ]
