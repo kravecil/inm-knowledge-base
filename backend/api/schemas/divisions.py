919f744084dd9b7aa8f1cf277b7b1f7571c27272
kravetsiv <brightsky@yandex.ru>
2025-05-27 18:46:57 +0500

    Исправлены поля схемы подразделения: id, parentId

--- backend/api/schemas/divisions.py
+++ backend/api/schemas/divisions.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel, Field
 
 
 class DivisionSchema(BaseModel):
-    GUID: UUID = Field(validation_alias="GUID")
-    name: str = Field(validation_alias="Name")
-    parent_guid: UUID = Field(validation_alias="ParentGuid")
+    GUID: UUID = Field(validation_alias="GUID", serialization_alias="id")
+    name: str = Field(validation_alias="Name", serialization_alias="name")
+    parent_guid: UUID = Field(
+        validation_alias="ParentGuid", serialization_alias="parentId"
+    )
